072c55ba98fe38fa9edb5fee7ed1430cdf8bbb5f
bmharvey <bmharvey@tamu.edu>
2023-04-03 21:28:25 -0500

    minor changes and documentation

--- keymap.c
+++ keymap.c
@@ -58,7 +58,7 @@
  * .-----------------------------.       .-----------------------------.  |
  * |     |     |     | F11 | F12 |       |     |     |     |     |     |  |
  * |-----------------------------|       |-----------------------------|  |
- * |  F1 |  F2 |  F3 |  F4 |  F5 |       |     | alt |     |     |     |  |
+ * |  F1 |  F2 |  F3 |  F4 |  F5 |       |     |ctl [| alt |ctl ]|     |  |
  * |-----------------------------|       |-----------------------------|  |
  * |  F6 |  F7 |  F8 |  F9 |  F0 |       |     |     |     |     |     |  |
  * '-----------------------------'       '-----------------------------'  |
@@ -193,17 +193,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [GAME_LAYER] = LAYOUT(
     // --------------------------------------+--------------------------------------------------------------
     KC_TAB  , KC_Q , KC_W , KC_E , KC_R , /* | */ KC_VOLU , LCTL(KC_EQL) , KC_WH_U , LCTL(KC_MINS) , KC_NO ,
-    KC_LSFT , KC_A , KC_S , KC_D , KC_F , /* | */ KC_MUTE , KC_WH_L      , KC_WH_D , KC_WH_R       , KC_NO        ,
-    KC_LCTL , KC_Z , KC_X , KC_C , KC_V , /* | */ KC_VOLD , KC_MPRV      , KC_MPLY , KC_MNXT       , KC_NO        ,
+    KC_LSFT , KC_A , KC_S , KC_D , KC_F , /* | */ KC_MUTE , KC_WH_L      , KC_WH_D , KC_WH_R       , KC_NO ,
+    KC_LCTL , KC_Z , KC_X , KC_C , KC_V , /* | */ KC_VOLD , KC_MPRV      , KC_MPLY , KC_MNXT       , KC_NO ,
     // --------------------------------------+--------------------------------------------------------------
                         KC_ESC , KC_SPC , /* | */ KC_NO , TO(BASE_LAYER)
     // --------------------------------------+--------------------------------------------------------------
   ),
   [FXN_LAYER] = LAYOUT(
     // ------------------------------------------+--------------------------------------------------------------
-    KC_NO , KC_NO , KC_NO , KC_F11 , KC_F12 , /* | */ KC_NO , KC_NO   , KC_NO , KC_NO , KC_NO   ,
-    KC_F1 , KC_F2 , KC_F3 , KC_F4  , KC_F5  , /* | */ KC_NO , KC_LALT , KC_NO , KC_NO , KC_NO   ,
-    KC_F6 , KC_F7 , KC_F8 , KC_F9  , KC_F10 , /* | */ KC_NO , KC_NO   , KC_NO , KC_NO , KC_NO   ,
+    KC_NO , KC_NO , KC_NO , KC_F11 , KC_F12 , /* | */ KC_NO , KC_NO         , KC_NO   , KC_NO         , KC_NO ,
+    KC_F1 , KC_F2 , KC_F3 , KC_F4  , KC_F5  , /* | */ KC_NO , LCTL(KC_LBRC) , KC_LALT , LCTL(KC_RBRC) , KC_NO ,
+    KC_F6 , KC_F7 , KC_F8 , KC_F9  , KC_F10 , /* | */ KC_NO , KC_NO         , KC_NO   , KC_NO         , KC_NO ,
     // ------------------------------------------+--------------------------------------------------------------
                               KC_NO , KC_NO , /* | */ KC_NO , TO(BASE_LAYER)
     // ------------------------------------------+--------------------------------------------------------------
